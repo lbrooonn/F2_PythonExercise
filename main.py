607c09efe027c3c5a345c73749bc999b42ef65b3
Le Bronn <lebronn.samson@cit.edu>
2023-09-04 14:59:00 +0800

    Final commit

--- main.py
+++ main.py
@@ -15,11 +15,11 @@ while True:
             num2 = int(input("Enter number 2: "))
             num3 = int(input("Enter number 3: "))
 
-            if(num1 > num2 and num2 > num3):
+            if num1 > num2 and num2 > num3:
                 print(f"The maximum number among the three is {num1}.")
-            elif(num2 > num1 and num2 > num3):
+            elif num2 > num1 and num2 > num3:
                 print(f"The maximum number among the three is {num2}.")
-            elif(num3 > num1 and num3 > num2):
+            elif num3 > num1 and num3 > num2:
                 print(f"The maximum number among the three is {num3}.")
             else:
                 print("All numbers are the same!")
@@ -36,14 +36,14 @@ while True:
                 smallestNum = 0
                 highestNum = 0
 
-            while(givenNum != 0):
+            while givenNum != 0:
                 slicedNum = givenNum % 10
                 numDigits += 1
 
-                if(slicedNum < smallestNum):
+                if slicedNum < smallestNum:
                     smallestNum = slicedNum
 
-                if(slicedNum > highestNum):
+                if slicedNum > highestNum:
                     highestNum = slicedNum
 
                 givenNum //= 10
@@ -52,6 +52,7 @@ while True:
             print("Smallest number is: ", smallestNum)
             print("Highest number is: ", highestNum)
         case 4:
+        #Number 4
             num = int(input("Enter a number: "))
             sum = 0
 
@@ -59,6 +60,77 @@ while True:
                 sum += totalNum
 
             print(f"The sum of all numbers from 1 to {num} is: ", sum)
+        case 5:
+        #Number 5
+            def decToBinary(dec):
+                if dec == 0:
+                    return '0'
+
+                binaryNum = ''
+
+                while dec > 0:
+                    binaryNum = str(dec % 2) + binaryNum
+                    dec //= 2
+
+                return binaryNum
+
+            def binaryToN(bin, type):
+                if type < 2:
+                    return "Invalid base number!"
+
+                decNum = 0
+                base = 0
+
+                for num in reversed(bin):
+                    decNum += int(num) * (type ** base)
+                    base += 1
+
+                return decNum
+
+            def decToOctal(dec):
+                if dec == 0:
+                    return '0'
+
+                octNum = ''
+
+                while dec > 0:
+                    octNum = str(dec % 8) + octNum
+                    dec //= 8
+
+                return octNum
+
+            def decToHex(dec):
+                if dec == 0:
+                    return '0'
+
+                hexChar = '0123456789ABCDEF'
+                hexString = ''
+
+                while dec > 0:
+                    separated = dec % 16
+                    hexString = hexChar[separated] + hexString
+                    dec //= 16
+
+                return hexString
+
+            def main():
+                decimalNum = int(input("Enter a decimal number: "))
+                binary = decToBinary(decimalNum)
+                octal = decToOctal(decimalNum)
+                hex = decToHex(decimalNum)
+
+                print("Binary: ", binary)
+                print("Octal: ", octal)
+                print("Hexadecimal: ", hex)
+        case 0:
+            print("Program exiting...")
+            break
+
+
+
+
+
+
 
 
 
