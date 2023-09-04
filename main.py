23ca6310fb6607504fbaa8704947374edd2f65b1
Le Bronn <lebronn.samson@cit.edu>
2023-09-04 14:39:24 +0800

    Second commit

--- main.py
+++ main.py
@@ -40,10 +40,10 @@ while True:
                 slicedNum = givenNum % 10
                 numDigits += 1
 
-                if(slicedNum < givenNum):
+                if(slicedNum < smallestNum):
                     smallestNum = slicedNum
 
-                if(slicedNum > givenNum):
+                if(slicedNum > highestNum):
                     highestNum = slicedNum
 
                 givenNum //= 10
@@ -51,6 +51,14 @@ while True:
             print("Number of digits in the given number: ", numDigits)
             print("Smallest number is: ", smallestNum)
             print("Highest number is: ", highestNum)
+        case 4:
+            num = int(input("Enter a number: "))
+            sum = 0
+
+            for totalNum in range(1, num + 1):
+                sum += totalNum
+
+            print(f"The sum of all numbers from 1 to {num} is: ", sum)
 
 
 
